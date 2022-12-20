5126aaf234acf130a2f671f519f1fcea1dd1fb48
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2022-12-21 03:05:30 +0900

    Master branch Cors 문제 해결 (#24)
    * 유저 기초설계 (#6)
    
    * 유저 기초설계
    
    * 유저 기초설계 수정 사항 적용
    
    * 유저 정보 가져오기 구현 (#8)
    
    * 유저 정보 가져오기 구현
    
    * 유저목록 수정사항 반영
    
    * Feat #5주식정보entity설계 (#10)
    
    * [추가] 주식정보Entity 추가
    
    * [수정] 코드 띄어쓰기 수정
    
    * [수정] EntityAuto increment 추가 (#11)
    
    * 유저 등록 (#9)
    
    * 유저 등록
    
    * 예외처리 구현
    
    * 예외 수정
    
    * .
    
    * 목록 가져오기 예외처리 (#13)
    
    * Feat #7user테스트코드작성 (#12)
    
    * 임시
    
    test
    
    * test: 테스트 코드 구현
    
    findUser, findAllUser 성공시 테스트 코드 구현
    
    * style: 개행 수정
    
    줄간격 및 개행 수정했습니다
    
    * 유저 찾기 성공 테스트 (#14)
    
    * 유저 가져오기 실패 테스트 (#16)
    
    * 유저등록테스트 (#17)
    
    * Feat #7유저레파지토리테스트 (#21)
    
    * .
    
    * test: 유저 등록 H2-database에서 mysql로 변경
    
    * refactor: Entity 생성을 메서드로 분리
    
    * fix: 테스트 in-memory 방식의 h2로 변경
    
    * test: 모든 유저 목록조회 테스트 완성
    
    * test: 특정 유저 조회 테스트 작성
    
    Co-authored-by: jiwon <pg15320@gmail.com>
    
    * feat: CORS 모든 허용 설정 (#23)
    
    Co-authored-by: jiwon <83341978+jiione@users.noreply.github.com>
    Co-authored-by: HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
    Co-authored-by: 임강호 <55732952+Icln@users.noreply.github.com>
    Co-authored-by: jiwon <pg15320@gmail.com>

---
+++ src/test/java/com/mtg/Motugame/user/repository/UserRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.mtg.Motugame.user.repository;
+
+import com.mtg.Motugame.entity.UserEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("h2")
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("특정 유저 조회")
+    @Test
+    void findByUserId() {
+        //given
+        userRepository.save(user());
+
+        //when
+        Optional<UserEntity> findUser = userRepository.findById("qwd5320");
+
+        //then
+        assertThat(findUser.get().getName()).isEqualTo(user().getName());
+    }
+
+    @DisplayName("유저 목록 조회")
+    @Test
+    void findAllUser() {
+        //given
+        userRepository.save(user());
+
+        //when
+        List<UserEntity> userList = userRepository.findAll();
+
+        //then
+        assertThat(userList.size()).isEqualTo(1);
+    }
+
+    @DisplayName("유저 등록")
+    @Test
+    void insertUser() {
+        //given
+        UserEntity user = user();
+
+        //when
+        UserEntity savedUser = userRepository.save(user);
+
+        // then
+        assertThat(savedUser.getId()).isEqualTo(user.getId());
+        assertThat(savedUser.getName()).isEqualTo(user.getName());
+        assertThat(savedUser.getGameId()).isEqualTo(user.getGameId());
+    }
+
+    private UserEntity user() {
+        return new UserEntity("qwd5320", "jiwon", "jiione");
+    }
+}
