7318b82fa2e748f463e29b18f924bb81adeab6d7
HanSeoJin <yp6484@gmail.com>
2022-12-21 02:56:17 +0900

    [추가] StockPrice Repository 추가

---
+++ src/main/java/com/mtg/Motugame/stock/repository/StockPriceRepository.java
@@ -0,0 +1,7 @@
+package com.mtg.Motugame.stock.repository;
+
+import com.mtg.Motugame.entity.StockPriceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockPriceRepository extends JpaRepository<StockPriceEntity, String> {
+}
