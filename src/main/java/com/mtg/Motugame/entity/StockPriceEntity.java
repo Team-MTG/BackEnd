2a279180b4515ef07a3a0f2210c64741d802eb84
HanSeoJin <yp6484@gmail.com>
2022-12-20 17:49:06 +0900

    [추가] 주식정보Entity 추가

---
+++ src/main/java/com/mtg/Motugame/entity/StockPriceEntity.java
@@ -0,0 +1,28 @@
+package com.mtg.Motugame.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Getter
+@Table(name = "stock_price")
+public class StockPriceEntity {
+    @Id
+    private Long id;
+    //FK
+    @ManyToOne
+    @JoinColumn(name = "stock_code")
+    private StockInfoEntity stockInfo;
+    private Date date;
+    private Integer open;
+    private Integer high;
+    private Integer low;
+    private Integer close;
+    private Float changePrice;
+}
