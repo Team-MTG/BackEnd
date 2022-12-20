ede36ad97961c07383a316167c85ea40499866b2
jiwon <pg15320@gmail.com>
2022-12-20 18:19:31 +0900

    예외 수정

--- src/main/java/com/mtg/Motugame/exception/ApiExceptionHandler.java
+++ src/main/java/com/mtg/Motugame/exception/ApiExceptionHandler.java
@@ -12,20 +12,11 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
-        if(e.getMessage().equals(ExceptionMessage.USER_ALREADY_EXISTS)){
-            HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-
-            ApiException apiException = new ApiException(
-                    ExceptionMessage.USER_ALREADY_EXISTS,
-                    httpStatus);
-
-            return new ResponseEntity<>(apiException, httpStatus);
-        }
 
         HttpStatus httpStatus =HttpStatus.BAD_REQUEST;
 
         ApiException apiException = new ApiException(
-                ExceptionMessage.NO_DATA_ERROR,
+                e.getMessage(),
                 httpStatus);
 
         return new ResponseEntity<>(apiException,httpStatus);
