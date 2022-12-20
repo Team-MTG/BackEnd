504fbc7acab723e0db2a9f34cdd8fed890dcb7d7
kwYoohae <gocksdl7979@gmail.com>
2022-12-20 12:53:14 +0900

    feat: 프로젝트 초기 구성

---
+++ src/main/java/com/mtg/Motugame/MotugameApplication.java
@@ -0,0 +1,13 @@
+package com.mtg.Motugame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotugameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotugameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtg/Motugame/MotugameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtg.Motugame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotugameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
