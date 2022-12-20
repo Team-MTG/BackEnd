f172b80d017e7940b273f915f7325758d2567b68
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2022-12-21 03:24:52 +0900

    Feat #15랜덤주식데이터찾기구현 (#25)
    * [수정] entity 날짜 타입변경
    
    * [추가] 주식정보 DTO 추가
    
    * [추가] 주식 가격정보 DTO 추가
    
    * [추가] StockController 구현
    
    * [추가] Stock Repository 구현
    
    * [추가] StockPrice Repository 추가
    
    * [추가] StockService 인터페이스 구현
    
    * [추가] stockService 구현
    
    * [수정] Collection 변경
    
    * [수정] 코드 띄어쓰기 수정

---
+++ src/main/java/com/mtg/Motugame/stock/repository/StockInfoRepository.java
@@ -0,0 +1,8 @@
+package com.mtg.Motugame.stock.repository;
+
+import com.mtg.Motugame.entity.StockInfoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockInfoRepository extends JpaRepository<StockInfoEntity, String> {
+
+}
---
+++ src/main/java/com/mtg/Motugame/stock/service/StockService.java
@@ -0,0 +1,9 @@
+package com.mtg.Motugame.stock.service;
+
+import com.mtg.Motugame.stock.dto.StockDataInfoDto;
+
+import java.util.List;
+
+public interface StockService {
+    public List<StockDataInfoDto> getStocksPrices(List<Integer> index);
+}
