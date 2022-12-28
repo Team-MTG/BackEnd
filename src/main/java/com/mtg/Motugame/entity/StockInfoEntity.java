1765ac689e543e489d7352a14d7ffc310ed0f8a6
kwYoohae <gocksdl7979@gmail.com>
2022-12-28 13:37:28 +0900

    feat: stock_info 테이블 UNIQUE constraint 추가 및 length 지정

--- src/main/java/com/mtg/Motugame/entity/StockInfoEntity.java
+++ src/main/java/com/mtg/Motugame/entity/StockInfoEntity.java
@@ -11,13 +11,13 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Getter
-@Table(name = "stock_info")
+@Table(name = "stock_info", uniqueConstraints = {@UniqueConstraint(name = "UNIQUE_STOCKNAME", columnNames = {"stock_name"})})
 public class StockInfoEntity {
     @Id
-    @Column(name = "stock_code")
+    @Column(name = "stock_code", length = 30)
     private String stockCode;
 
-    @Column(name = "stock_name")
+    @Column(name = "stock_name", length = 50, nullable = false)
     private String stockName;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "stockInfo")
