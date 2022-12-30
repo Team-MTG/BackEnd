29afda17eedaaadff634e9723c5414596cca5a48
kwYoohae <gocksdl7979@gmail.com>
2022-12-30 18:59:48 +0900

    refactor: 상속으로만 쓰이기 때문에 abstract class로 변경

--- src/main/java/com/mtg/Motugame/entity/CreatedTimeEntity.java
+++ src/main/java/com/mtg/Motugame/entity/CreatedTimeEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class CreatedTimeEntity {
+public abstract class CreatedTimeEntity {
 
     @CreatedDate
     @Column(name = "created_at")
