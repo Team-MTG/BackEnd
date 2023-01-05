25e2544105f12a69539809570b89c29b521a950f
jiwon <83341978+jiione@users.noreply.github.com>
2023-01-05 15:37:29 +0900

    랭킹 서비스 테스트 (#33)

--- src/main/java/com/mtg/Motugame/ranking/service/RankingServiceImpl.java
+++ src/main/java/com/mtg/Motugame/ranking/service/RankingServiceImpl.java
@@ -38,7 +38,6 @@ public class RankingServiceImpl implements RankingService{
                     .rate(ranked.getRate())
                     .totalCash(ranked.getTotalCash())
                     .build());
-
         }
 
         return list;
