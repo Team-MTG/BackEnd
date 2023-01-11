02f0ba8c7e7e9ccdfbf2fef995703a3ea7f9f790
kwYoohae <gocksdl7979@gmail.com>
2023-01-12 05:22:33 +0900

    fix: stockPrice Entity에 대해서 Repository의 타입이 잘못 매핑되었기 때문에 변경

--- src/main/java/com/mtg/Motugame/stock/repository/StockPriceRepository.java
+++ src/main/java/com/mtg/Motugame/stock/repository/StockPriceRepository.java
@@ -3,5 +3,5 @@ package com.mtg.Motugame.stock.repository;
 import com.mtg.Motugame.entity.StockPriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockPriceRepository extends JpaRepository<StockPriceEntity, String> {
+public interface StockPriceRepository extends JpaRepository<StockPriceEntity, Long> {
 }
