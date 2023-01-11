b1a488884703d1a3e962066286c2d42e1d55ca1a
kwYoohae <gocksdl7979@gmail.com>
2023-01-12 01:31:06 +0900

    [추가] stock Dto추가

---
+++ src/main/java/com/mtg/Motugame/stock/dto/StockDataInfoDto.java
@@ -0,0 +1,8 @@
+package com.mtg.Motugame.stock.dto;
+
+import java.util.List;
+
+public class StockDataInfoDto {
+    private List<StockAverageDto> stockAverages;
+    private List<StockPriceDto> stockPrices;
+}
