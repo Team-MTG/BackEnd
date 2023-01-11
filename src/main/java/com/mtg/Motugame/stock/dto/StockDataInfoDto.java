c9d4ae7b8a27cc20e63c2db7e2b23e372ecd2a3e
HanSeoJin <yp6484@gmail.com>
2023-01-12 06:15:35 +0900

    [수정] 어노테이션 추가

--- src/main/java/com/mtg/Motugame/stock/dto/StockDataInfoDto.java
+++ src/main/java/com/mtg/Motugame/stock/dto/StockDataInfoDto.java
@@ -1,7 +1,12 @@
 package com.mtg.Motugame.stock.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@Builder
 public class StockDataInfoDto {
     private List<StockAverageDto> stockAverages;
     private List<StockPriceDto> stockPrices;
