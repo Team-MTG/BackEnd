4231e4c9eea59e6c7c43de8498921848e82f671d
kangholim <55732952+Icln@users.noreply.github.com>
2023-01-19 15:40:43 +0900

    fix: RankingRespository삭제

--- src/main/java/com/mtg/Motugame/ranking/repository/RankingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mtg.Motugame.ranking.repository;
-
-import com.mtg.Motugame.entity.RankingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankingRepository extends JpaRepository<RankingEntity,Long> {
-}
