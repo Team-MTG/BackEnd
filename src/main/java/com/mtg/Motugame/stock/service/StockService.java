62dfb5fac2c3d1386df4c19ac1efcfddaa766f10
HanSeoJin <yp6484@gmail.com>
2023-01-22 22:20:23 +0900

    [수정] Service 사용 메서드 수정

--- src/main/java/com/mtg/Motugame/stock/service/StockService.java
+++ src/main/java/com/mtg/Motugame/stock/service/StockService.java
@@ -1,11 +1,11 @@
 package com.mtg.Motugame.stock.service;
 
 import com.mtg.Motugame.stock.dto.StockAverageDto;
+import com.mtg.Motugame.stock.dto.StockDataInfoDto;
 import com.mtg.Motugame.stock.dto.StockPriceDto;
 
 import java.util.List;
 
 public interface StockService {
-    List<StockPriceDto> getStocksPrices(List<String> seeds);
-    List<StockAverageDto> getAveragePrices(List<String> seeds);
+    StockDataInfoDto getStockDataInfoDto(List<String> seeds);
 }
